4e4eafd3b10fe98d6b29800160bfe8c55db35fd0
wsmidt <scarab9151@gmail.com>
2020-11-25 18:11:12 -0800

    added tmux to the setup script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -9,7 +9,6 @@
 #TODO [wsmidt]
 # - loops (mkdirs, apt-gets, etc...)
 # - skip installs if file already exists. (example curl installs)
-# - idea tar extracts the tgz into idea-IC-202.7660.26. this should probably be moved t normalized dir to prevent issues with different version if ever updated.
 
 #setup vars
 BIN_DIR=~/bin
@@ -43,7 +42,18 @@ IDEA_TGZ="$IDEA_VERSION.tar.gz"
 IDEA_DOWNLOAD_URL=https://download.jetbrains.com/idea/$IDEA_TGZ
 IDEA_DOWNLOAD_FILE=$DOWNLOAD_DIR/$IDEA_TGZ
 IDEA_LIB_DIR=$LIB_DIR/$IDEA_VERSION
-curl -L $IDEA_DOWNLOAD_URL -o $IDEA_DOWNLOAD_FILE
-mkdir -p $IDEA_LIB_DIR
-tar -xvzf $IDEA_DOWNLOAD_FILE -C $IDEA_LIB_DIR
-ln -s $IDEA_LIB_DIR/idea-IC-202.7660.26/bin/idea.sh ~/bin/idea.sh
+if [ ! -d "$IDEA_LIB_DIR" ]; then
+  curl -L $IDEA_DOWNLOAD_URL -o $IDEA_DOWNLOAD_FILE
+  mkdir -p $IDEA_LIB_DIR
+  tar -xvzf $IDEA_DOWNLOAD_FILE -C $IDEA_LIB_DIR
+  ln -fs $IDEA_LIB_DIR/idea-IC-202.7660.26/bin/idea.sh ~/bin/idea.sh
+fi
+
+#install tmux
+sudo apt-get install tmux -y
+NORD_TMUX_PLUGIN_DIR=~/.tmux/themes/nord-tmux
+if [ ! -f "$NORD_TMUX_PLUGIN_DIR/nord.tmux" ]; then
+  mkdir -p $NORD_TMUX_PLUGIN_DIR
+  git clone git@github.com:arcticicestudio/nord-tmux.git $NORD_TMUX_PLUGIN_DIR
+fi
+cp `dirname $0`/tmux.conf ~/.tmux.conf
